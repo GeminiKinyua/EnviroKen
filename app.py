13031f4d51e2e1f88126b668d2d787ff07ba19f3
Manasseh <manassehkinyua02@gmail.com>
2024-06-06 04:46:11 +0300

    Update

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
 import streamlit as st
 import numpy as np
-import keras
 import tensorflow as tf
 from tensorflow.keras.utils import load_img, img_to_array
-from keras.applications.vgg16 import preprocess_input
+from tensorflow.keras.applications.vgg16 import preprocess_input
 from PIL import Image
 
 def load_model():
@@ -13,18 +12,18 @@ def load_model():
 def predict_image(model, img):
     img = img_to_array(img)
     img = np.expand_dims(img, axis=0)
-    img = img/255.0 
+    img = img / 255.0
     prediction = model.predict(img)
     class_index = np.argmax(prediction, axis=1)
     return class_index
 
-
 st.title("ENVIROKEN")
-st.write("This is a simple image classification web app that predicts the waste type")
-st.write("The goal is to integrate a reward system that determines a reward based on the predicted class")
+st.write("This is a simple image classification web app that predicts the waste type.")
+st.write("The goal is to integrate a reward system that determines a reward based on the predicted class.")
+
 model = load_model()
 
-class_labels = ['e-Waste', 'Glass', 'Metal', 'Organic','Paper', 'Plastic']
+class_labels = ['e-Waste', 'Glass', 'Metal', 'Organic', 'Paper', 'Plastic']
 
 uploaded_file = st.file_uploader("Upload an image", type=["jpg", "png"])
 
@@ -39,7 +38,7 @@ if uploaded_file is not None:
 
     # Reward based on class
     if st.button('Get Reward'):
-        if class_labels[label[0]] == 'e-waste':
+        if class_labels[label[0]] == 'e-Waste':
             reward = 'You get reward 1!'
         elif class_labels[label[0]] == 'Glass':
             reward = 'You get reward 2!'
@@ -53,6 +52,5 @@ if uploaded_file is not None:
             reward = 'You get reward 6!'
         else:
             reward = 'You get no reward!'
-
 
         st.write(reward)
